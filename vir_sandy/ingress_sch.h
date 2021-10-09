a554b5c2a96878002364519f90abd981e504e801
“sandy533533” <sandy_6536@163.com>
2021-10-09 17:03:26 +0800

    add process

--- vir_sandy/ingress_sch.h
+++ vir_sandy/ingress_sch.h
@@ -13,17 +13,16 @@ struct ingress_sch : sc_module {
 
 
     int                         m_cycle_cnt;   
-    deque<TRANS>                *input_que;
+    deque<TRANS>                input_que;
    
 
     void                        main_process();
     void                        recv_packet_process();
-//    void                        sch_process();
+    void                        sch_process();
 
-  comm_stat_bw                   *m_bw_stat;
-  global_config_c                *m_cfg;
-
- //   RR_SCH                      *rr_sch; 
+    comm_stat_bw                *m_bw_stat;
+    global_config_c             *m_cfg;
+    RR_SCH                      *rr_sch; 
 
 
     SC_CTOR(ingress_sch) 
@@ -31,7 +30,7 @@ struct ingress_sch : sc_module {
 
 //    rr_sch = new RR_SCH(0);
       input_fifo = new  sc_in<TRANS>() ;
-      input_que  = new  deque<TRANS>() ;
+ //     input_que  = new  deque<TRANS>() ;
 
       m_cycle_cnt =0;
       m_cfg = new global_config_c();
@@ -40,7 +39,7 @@ struct ingress_sch : sc_module {
       string debug_file = string("ingress_sach_debug.log");
       m_bw_stat =new comm_stat_bw(m_cfg, debug_file,g_m_inter_num);
 
- //     rr_sch = new RR_SCH(g_m_inter_num);
+      rr_sch = new RR_SCH(g_m_inter_num);
 
 
        SC_METHOD(main_process);
